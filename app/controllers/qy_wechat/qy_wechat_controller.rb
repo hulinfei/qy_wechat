2ceab8af31bdb2e4e9761166dc65de28bb780083
hlf <32318483+hulinfei@users.noreply.github.com>
2018-10-22 18:08:50 +0800

    Update qy_wechat_controller.rb

--- app/controllers/qy_wechat/qy_wechat_controller.rb
+++ app/controllers/qy_wechat/qy_wechat_controller.rb
@@ -4,7 +4,7 @@ module QyWechat
 
     include ReplyMessageHelper
 
-    #skip_before_action :verify_authenticity_token, only: :reply
+    # skip_before_action :verify_authenticity_token, only: :reply
     before_action :setup_qy_app, only: [:verify_url, :reply]
     before_action :setup_wechat_message, only: :reply
 
